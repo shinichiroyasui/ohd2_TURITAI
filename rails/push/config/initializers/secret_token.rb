2476dba2db0847506203962fdcc9f8d28a2b47d7
Kohei Tomita <tommy.fmale@gmail.com>
2014-02-15 16:50:28 +0900

    push通信用のプロジェクト作成

---
+++ rails/push/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Push::Application.config.secret_token = '42da3651ba9457bd4e2c50a14114557914dd4f46cc04299c545c45ffea9c0975a4d0c5ea541bb8685e58373f39e72bc96ed625e0fbb87a4674b87b4b0c564893'
