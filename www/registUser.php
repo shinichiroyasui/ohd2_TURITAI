187670a4d6ba53e2a30235079f89160216b939c3
Ubuntu <ubuntu@ip-172-31-10-69.us-west-2.compute.internal>
2014-02-15 18:05:19 +0000

    update registUser

--- www/registUser.php
+++ www/registUser.php
@@ -1,6 +1,7 @@
 <?php
 $token = $_GET['token'];
 $gkey = $_GET['googlekey'];
+$aid = $_GET['android_id'];
 $token = "";
 //$res = request("https://graph.facebook.com/me?access_token=".$token);
 updateUserDB($token,"");
@@ -22,7 +23,7 @@ function request($url){
 //FBからユーザーデータを取得してuserdbを更新
 function updateUserDB($token,$gkey){
 	try{
-		$dbh = new PDO('mysql:host=localhost;dbname=taituri', 'root', '');
+		$dbh = new PDO('mysql:host=localhost;dbname=turitai', 'pixy', 'pass');
 	} catch (PDOException $e) {
 		echo "failed pdo";
 	}
@@ -38,58 +39,100 @@ function updateUserDB($token,$gkey){
 	$data = json_decode($json);
 	var_dump($data);
 	$work = array();
+	$ramen = 0;
+	$museum = 0;
 	foreach($data->data as $v){
-		$place_id=$v->place->id;
 		$place_name=preg_replace("/'/","//",$v->place->name);
-		$sql = "INSERT INTO pois (poiid,name) VALUES ('$place_id','$place_name')";
-		array_push($work,$place_id);
-
-		$st = $pdo->prepare("INSERT INTO pois (poiid,name) VALUES (:id,:name)");
-		$st->bindParam(':name',$place_name);
-		$st->bindParam(':id',$place_id);
+		$place_id=$v->place->id;
+		$sql = "SELECT * from pois where poiid='$place_id'";
+		$st = $pdo->query($sql);
 		$st->execute();
-	}
-	$pois = implode(",",$work);
+		$cnt=0;
+		while ($row = $st->fetch()) {
+			$cnt++;
+		}
+		if($cnt==0){
+			$place_name=preg_replace("/'/","//",$v->place->name);
+			$sql = "INSERT INTO pois (poiid,name) VALUES ('$place_id','$place_name')";
+			array_push($work,$place_id);
 
-
-	$url = "https://graph.facebook.com/me/movies?access_token=".$token;
-	$json = request($url);
-	$data = json_decode($json);
-	var_dump($data);
-	$work = array();
-	foreach($data->data as $v){
-		array_push($work,$v->id);
-	}
-	$movies = implode(",",$work);
-
-	$url = "https://graph.facebook.com/me/music?access_token=".$token;
-	$json = request($url);
-	$data = json_decode($json);
-	$work = array();
-	foreach($data->data as $v){
-		array_push($work,$v->id);
-	}
-	$musics = implode(",",$work);
-
-	$url = "https://graph.facebook.com/me/books?access_token=".$token;
-	$json = request($url);
-	$data = json_decode($json);
-	$work = array();
-	foreach($data->data as $v){
-		array_push($work,$v->id);
-	}
-	$books = implode(",",$work);
-	$st = $pdo->prepare("INSERT INTO users (userid,googlekey,accesstoken,sex,birthday,books,movies,pois,musics) VALUES (':id',':gkey',':token',:sex,':birthday','books',':movies',':pois',':musics')");
-		$st->bindParam(':id',$userid);
-		$st->bindParam(':gkey',$gkey);
-		$st->bindParam(':token',$token);
-		$st->bindParam(':sex',$sex);
-		$st->bindParam(':birthday',$birthday);
-		$st->bindParam(':books',$books);
-		$st->bindParam(':movies',$movies);
-		$st->bindParam(':pois',$pois);
-		$st->bindParam(':musics',$musics);
+			$st = $pdo->prepare("INSERT INTO pois (poiid,name) VALUES (:id,:name)");
+			$st->bindParam(':name',$place_name);
+			$st->bindParam(':id',$place_id);
+			$st->execute();
+		}
+		$pois = implode(",",$work);
+		$sql = "SELECT * from userspots where userid='$userid' and poiid='$place_id' ";
+		$st = $pdo->query($sql);
 		$st->execute();
-	return $pois;
+		$cnt=0;
+		while ($row = $st->fetch()) {
+			$cnt++;
+		}
+		if($cnt==0){
+			$place_name=preg_replace("/'/","//",$v->place->name);
+			$sql = "INSERT INTO userspots (poiid,name) VALUES ('$place_id','$place_name')";
+			array_push($work,$place_id);
+
+			$st = $pdo->prepare("INSERT INTO userspots (userid,poiid) VALUES (:uid,:pid)");
+			$st->bindParam(':uid',$user_id);
+			$st->bindParam(':pid',$place_id);
+			$st->execute();
+
+		}
+
+
+		$url = "https://graph.facebook.com/me/movies?access_token=".$token;
+		$json = request($url);
+		$data = json_decode($json);
+		var_dump($data);
+		$work = array();
+		foreach($data->data as $v){
+			array_push($work,$v->id);
+		}
+		$movies = implode(",",$work);
+
+		$url = "https://graph.facebook.com/me/music?access_token=".$token;
+		$json = request($url);
+		$data = json_decode($json);
+		$work = array();
+		foreach($data->data as $v){
+			array_push($work,$v->id);
+		}
+		$musics = implode(",",$work);
+
+		$url = "https://graph.facebook.com/me/books?access_token=".$token;
+		$json = request($url);
+		$data = json_decode($json);
+		$work = array();
+		foreach($data->data as $v){
+			array_push($work,$v->id);
+		}
+		$books = implode(",",$work);
+		$sql = "SELECT * from users where android_id='$aid'";
+		$st = $pdo->query($sql);
+		$st->execute();
+		$cnt=0;
+		while ($row = $st->fetch()) {
+			$cnt++;
+		}
+		if($cnt==0){
+			$st = $pdo->prepare("INSERT INTO users (userid,googlekey,accesstoken,sex,birthday,books,movies,pois,musics,ramen,museum) VALUES (:id,:gkey,:token,:sex,:birthday,:books,:movies,:pois,:musics,:ramen,:museum')");
+			$st->bindParam(':id',$userid);
+			$st->bindParam(':gkey',$gkey);
+			$st->bindParam(':aid',$aid);
+			$st->bindParam(':token',$token);
+			$st->bindParam(':sex',$sex);
+			$st->bindParam(':birthday',$birthday);
+			$st->bindParam(':books',$books);
+			$st->bindParam(':movies',$movies);
+			$st->bindParam(':pois',$pois);
+			$st->bindParam(':musics',$musics);
+			$st->bindParam(':ramen',$ramen);
+			$st->bindParam(':museum',$museum);
+			$st->execute();
+		}
+	}
+	return true;
 }
 ?>
