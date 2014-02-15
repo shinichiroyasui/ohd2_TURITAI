12027e528a51b8bd6056c62c8c1c6247cb8eb30a
Kohei Tomita <tommy.fmale@gmail.com>
2014-02-15 21:38:51 +0900

    本, スポット, 音楽の好みの特徴を運命度に組み込み

--- rails/push/app/models/user.rb
+++ rails/push/app/models/user.rb
@@ -1,15 +1,26 @@
 class User < ActiveRecord::Base
-  attr_accessible :id, :googlekey, :birthday, :sex
-  SEX_MALE=0
-  SEX_FEMALE=1
+  attr_accessible :id, :googlekey, :birthday, :sex, :pois, :books, :musics
+  SEX_MALE = 0
+  SEX_FEMALE = 1
 
   # 特徴のキー
-  FEATURE_KEYS = [:same_birthday]
+  FEATURE_KEYS = [:same_birthday, :same_poi_rate, :same_book_rate, :same_music_rate]
+
+  # 特徴事の重み付け
   # ユーザ毎に異なる重み付けにする
-  FEATURE_WEIGHT = {:same_birthday => 1}
+  FEATURE_WEIGHT = {
+    same_birthday: 1,
+    same_poi_rate: 1,
+    same_book_rate: 1,
+    same_music_rate: 1
+  }
 
   def fortune_score(other_user)
     vector = convert_feature_vector(other_user)
+    logger.debug { "user1: #{self.inspect}" }
+    logger.debug { "user2: #{other_user.inspect}" }
+    logger.debug { "user3: #{vector.inspect}" }
+    # 重み付け線形和
     FEATURE_KEYS.sum { |key| vector[key] * FEATURE_WEIGHT[key] }
   end
 
@@ -17,6 +28,16 @@ private
   def convert_feature_vector(other_user)
     vector = {}
     vector[:same_birthday] = (self.birthday == other_user.birthday) ? 1 : 0
+    vector[:same_poi_rate] = same_value_rate(self.pois.split(","), other_user.pois.split(","))
+    vector[:same_book_rate] = same_value_rate(self.books.split(","), other_user.books.split(","))
+    vector[:same_music_rate] = same_value_rate(self.musics.split(","), other_user.musics.split(","))
     vector
+  end
+
+  # ジャッカード係数
+  def same_value_rate(array1, array2)
+    same_num = (array1 & array2).size
+    all_num = (array1 | array2).size
+    same_num.to_f / all_num
   end
 end
